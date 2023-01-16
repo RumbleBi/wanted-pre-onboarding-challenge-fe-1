ca88e367a7bc2a77e863952def99ec78cb311cda
RumbleBi <olzlel2000@gmail.com>
2023-01-16 22:00:36 +0900

    ADD: react-query 초기설정완료

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Global } from "@emotion/react";
 import { globalStyles } from "./styles/globalStyles";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <>
-    <Global styles={globalStyles} />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <Global styles={globalStyles} />
+      <ReactQueryDevtools initialIsOpen={true} />
+      <App />
+    </QueryClientProvider>
   </>
 );
